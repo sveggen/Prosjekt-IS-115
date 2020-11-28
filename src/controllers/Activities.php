be1e8e181e34ab955769a624dfe76b1cdcb00957
Markus Sveggen <marks18@uia.no>
2020-11-28 17:02:47 +0100

    added activity files

--- src/controllers/Activities.php
+++ src/controllers/Activities.php
@@ -4,13 +4,19 @@
 namespace App\controllers;
 
 
+use App\models\Activity;
 use Symfony\Component\HttpFoundation\Response;
 
 class Activities extends BaseController{
 
     public function activity() {
+
+        $activityModel = new Activity();
+        $allActivities = $activityModel->getAllFutureActivities();
+
         return new Response(
-            $this->twig->render('pages/activity/activity.html.twig')
+            $this->twig->render('pages/activity/activity.html.twig',
+            ['activities', $allActivities])
         );
 
     }
--- src/models/Activity.php
+++ src/models/Activity.php
@@ -4,7 +4,19 @@
 namespace App\models;
 
 
-class Activity extends Database
-{
+class Activity extends Database {
+
+    /**
+     * @return false|mysqli_result Containing MySQL of array containing
+     * all future activities.
+     */
+    public function getAllFutureActivities() {
+        $sql = "SELECT * FROM activity WHERE activity.start_time >= CURTIME() ORDER BY activity.start_time";
+        $stmt = $this->getConnection()->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $stmt->close();
+        return $result;
+    }
 
 }
